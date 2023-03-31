20e6c1620d37c1332bac6e05b77c49ead90f43f8
renqingyang <rq625230882@gmail.com>
2023-03-31 12:02:17 -0500

    db cluster

--- src/main/java/com/example/java3/week5/MicroserviceIntro.java
+++ src/main/java/com/example/java3/week5/MicroserviceIntro.java
@@ -22,7 +22,7 @@ package com.example.java3.week5;
  *      4. impl services with diff coding language
  *      5. message queue can take more requests..
  *      ...
- *  Challenges with Microservices (CAP / BASE)
+ *  Challenges with Microservices (CAP / BASE) / How to design Microservices
  *      1. communication
  *      2. test entire system
  *      3. documents
@@ -31,8 +31,30 @@ package com.example.java3.week5;
  *      6. database cluster / sharding / global transaction + cache clusters
  *      7. message queue
  *      8. take of ips / locations
+ *                          discovery service / service registration : [{department-service: [ip1, ip2, ip3]}, {employee-service: [ip4, ip5]}]
+ *                                  /                       \
+ *                  department-service      ->      employee-service
+ *
+ *          1. restTemplateProxy.getForObject(http://employee-service/, String.class);
+ *          2. fetch ips of employee-service from discovery service : ip4, ip5
+ *          3. ribbon -> load balance -> pick one from ip4, ip5
+ *          4. restTemplate.getForObject(http://ip4/, String.class);
+ *
  *      9. sync time for diff services
  *      10. api gateway service -> global unique id / rate limiter
+ *                          user
+ *                           |
+ *                          gateway -- security service
+ *                        /     \
+ *                     dept      account
+ *                     |
+ *                    emp
+ *
+ *           1. more secure
+ *           2. rate limiter
+ *           3. redirect request to diff service
+ *           4. log + co-relation id(global unique id)
+ *
  *      11. log -> centralize log -> analyze log -> build index -> provide searching function
  *      12. deployment (kubernetes / ECS / container management tools)
  *      13. CI/CD
@@ -40,7 +62,6 @@ package com.example.java3.week5;
  *      15. centralize properties
  *      ....
  *
- *  How to design Microservices
  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
  *  this afternoon office hour 2pm cdt
  *
